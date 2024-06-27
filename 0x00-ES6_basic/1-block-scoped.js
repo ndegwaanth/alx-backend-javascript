b6ae69a6cdd426aadc1c487c1c168582909b97f1
Anthony <ndegwaanthony300@gmail.com>
2024-06-27 18:08:39 +0300

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+import { transformAsync } from '@babel/core';
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
