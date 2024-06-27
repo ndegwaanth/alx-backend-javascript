2f02845c8bd9ed7264e4ae61431bfe29f504e5f8
Anthony <ndegwaanthony300@gmail.com>
2024-06-27 22:50:32 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // This task variable is confined to the if block
-    const task2 = false; // This task2 variable is confined to the if block
+    const task = true;
+    const task2 = false;
+    return [task2, task];
   }
 
   return [task, task2];
