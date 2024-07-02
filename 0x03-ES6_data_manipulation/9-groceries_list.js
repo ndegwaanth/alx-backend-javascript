22c57805fe87df9187d611de415b13ef0c92473e
Anthony <ndegwaanthony300@gmail.com>
2024-07-02 15:13:23 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const groceryMap = new Map();
+  
+  // Adding items to the Map
+  groceryMap.set('Apples', 10);
+  groceryMap.set('Tomatoes', 10);
+  groceryMap.set('Pasta', 1);
+  groceryMap.set('Rice', 1);
+  groceryMap.set('Banana', 5);
+
+  return groceryMap;
+}
