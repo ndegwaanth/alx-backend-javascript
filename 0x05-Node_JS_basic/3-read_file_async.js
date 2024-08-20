66d27c807be80bd8d65dbae02117612aab17530c
Anthony <ndegwaanthony300@gmail.com>
2024-08-21 00:48:01 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1).map(line => line.split(','));
+
+      const numberOfStudents = students.length;
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      const fields = {};
+      students.forEach(([firstname, lastname, age, field]) => {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
