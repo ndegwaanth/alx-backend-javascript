efb6ed577c7106814c53c9462b8de3a9d7fba158
Anthony <ndegwaanthony300@gmail.com>
2024-08-23 00:44:22 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,25 +1,36 @@
-const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+// test/2-calcul_chai.test.js
 
-describe('calculateNumber', function () {
-  describe('SUM', function () {
-    it('sum of 1.4 + 4.5 = 6', function () {
+const { expect } = require('chai');
+const calculateNumber = require('../2-calcul_chai');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
-  });
 
-  describe('SUBTRACT', function () {
-    it('Subtraction of 1.4 + 4.5 = -4', function () {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(4);
+    it('should return 6 when inputs are 2.6 and 3.3', function() {
+      expect(calculateNumber('SUM', 2.6, 3.3)).to.equal(6);
     });
   });
 
-  describe('DIVIDE', function () {
-    it('Divison of 1.4 / 4.5 = 0.2', function () {
+  describe('SUBTRACT', function() {
+    it('should return -3 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-3);
+    });
+
+    it('should return 0 when inputs are 3.0 and 3.0', function() {
+      expect(calculateNumber('SUBTRACT', 3.0, 3.0)).to.equal(0);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
       expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
-    it('division with zero 1.4 / 0 = Error', function () {
-      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+
+    it('should return "Error" when dividing by 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0.4)).to.equal('Error');
     });
   });
 });
