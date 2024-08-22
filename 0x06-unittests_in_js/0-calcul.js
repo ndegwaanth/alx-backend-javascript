631887117ebae41147360d444d71aa3d1d8e704e
Anthony <ndegwaanthony300@gmail.com>
2024-08-23 00:25:30 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,8 @@
-function calculateNumber(a, b){
+function calculateNumber (a, b) {
   const num1 = Math.round(a);
   const num2 = Math.round(b);
 
   return num1 + num2;
-};
+}
 
 module.exports = calculateNumber;
